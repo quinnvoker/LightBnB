b8518f8e310cdd6b9831aac41b752b44034f0eb3
quinnvoker <quinnvoker@gmail.com>
2020-08-19 13:29:13 -0700

    fixed some bugs

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE reservations (
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   property_id INTEGER REFERENCES property(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
