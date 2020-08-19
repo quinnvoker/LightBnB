a3208b4395e3c5cb5a25a06cd49f52f42cc81b53
quinnvoker <quinnvoker@gmail.com>
2020-08-19 13:31:27 -0700

    fixed some bugs

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  property_id INTEGER REFERENCES property(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -44,7 +44,7 @@ CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
