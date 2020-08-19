edc9ce9ca771d8576105250595b2ecf416aa96eb
Quinn <quinnvoker@gmail.com>
2020-08-19 22:02:00 +0000

    replaced query 5 with simpler version from compass

--- 1_queries/5_my_reservations.sql
+++ 1_queries/5_my_reservations.sql
@@ -1,24 +1,13 @@
-SELECT reservations.*, properties.*, average_ratings.rating AS average_rating
-  FROM
-    users
-    JOIN reservations ON users.id = reservations.guest_id
-    JOIN properties ON properties.id = reservations.property_id
-    JOIN property_reviews AS reviews ON users.id = reviews.guest_id
-    JOIN (
-      SELECT properties.id AS id, AVG(rev.rating) as rating
-        FROM 
-          properties
-          JOIN property_reviews rev ON properties.id = rev.property_id
-        GROUP BY
-          properties.id
-    ) AS average_ratings ON average_ratings.id = properties.id
-  WHERE
-    users.id = 1
-    AND reservations.end_date < NOW()::DATE
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM 
+    reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE 
+    reservations.guest_id = 1
+    AND reservations.end_date < now()::date
   GROUP BY 
-    properties.id, 
-    reservations.id, 
-    average_ratings.rating
-  ORDER BY
+    properties.id, reservations.id
+  ORDER BY 
     reservations.start_date
   LIMIT 10;
